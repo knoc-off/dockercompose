66734f4304d2782e5d5546885a73a182383ef7d8
niko <niko@skyline.sbs>
2022-09-14 11:57:41 +0000

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Deploy script for these server files
+# this should make sure that the computer has all needed depenedncies
+# by the end of this script you should have a fully working server 
+
+
+
+
